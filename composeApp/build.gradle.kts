e2868f0d491bf33bf297deb9d7db4fb66e18205f
Tamási Máté <matetamasi@protonmail.com>
2025-11-08 12:28:20 +0100

    Enforce trailing commas

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
 
     listOf(
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
