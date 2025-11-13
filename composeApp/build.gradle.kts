4975d2c5f6cbbf12073665281e6c6131fd0e77c5
László Görömbey <laszlo.gorombey@gmail.com>
2025-11-13 18:53:49 +0100

    Adds Navigation and Icons dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,6 +56,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.navigation.compose)
+            implementation(libs.material.icons.extended)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
