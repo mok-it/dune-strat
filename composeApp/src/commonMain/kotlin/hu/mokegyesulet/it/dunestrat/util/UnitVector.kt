bd18e791b81ff5d3d9708a67ad15c57f79a44ae1
cstomi74 <tomicsontos27@gmail.com>
2025-11-30 18:51:06 +0100

    This code generates the coordinates of the vertices of the hexagons on the map in SVG code, based on a list of tile ids (in the form of A1).

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/util/UnitVector.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/util/UnitVector.kt
@@ -4,8 +4,7 @@ data class UnitVector(
     val x: Int,
     val y: Int,
 ) {
-    fun toAbsolute(baseX: AbsoluteVector, baseY: AbsoluteVector): AbsoluteVector =
-        x * baseX + y * baseY
+
     operator fun times(number: Int): UnitVector = UnitVector(x * number, y * number)
 
     companion object { // ide beírni, hogy az A12-ből csináljon egy számpárt
@@ -25,38 +24,18 @@ data class AbsoluteVector(
         y + absvector.y,
     )
     operator fun times(number: Int): AbsoluteVector = AbsoluteVector(x * number, y * number)
-    companion object { // ide beírni, hogy az A12-ből csináljon egy számpárt
-        fun fromDisplayCoordinate(coord: String): UnitVector {
-            val first = coord.lowercase()[0].code - 'a'.code
-            val second = coord.substring(1).toInt() - 1 // így tesszük origóra
-            return UnitVector(first, second)
-        }
-    }
 }
 
 operator fun Int.times(absoluteVector: AbsoluteVector): AbsoluteVector = absoluteVector * this
 
-val vecX = AbsoluteVector(144.0, -83.13843876)
-val vecY = AbsoluteVector(0.0, 166.2768775)
-
 fun main() {
-    println(UnitVector.fromDisplayCoordinate("A3"))
-    val vectorX = UnitVector.fromDisplayCoordinate("A3").x
-    val vectorY = UnitVector.fromDisplayCoordinate("A3").y
-
-    val newVecX = vecX * vectorX
-    println("$newVecX")
-    val newVecY = vecY * vectorY
-    println("$newVecY")
-
-    val coords = listOf("C2")
+    val coords = listOf("A1", "A2", "B1", "B2", "C2", "E6")
     var s = ""
     for (c in coords) {
         s += hexDrawer(
             UnitVector.fromDisplayCoordinate(c),
-            newVecX,
-            newVecY,
+
         ) + "\n"
-        println("$s")
     }
+    println(s)
 }
