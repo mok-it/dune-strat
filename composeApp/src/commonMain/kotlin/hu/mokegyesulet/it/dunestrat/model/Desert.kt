ef588306cf507cea082593481c162af09e443dcb
László Görömbey <laszlo.gorombey@gmail.com>
2025-12-01 09:23:34 +0100

    WIP

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/model/Desert.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/model/Desert.kt
@@ -2,6 +2,7 @@ package hu.mokegyesulet.it.dunestrat.model
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlin.math.abs
 
 @Serializable
 data class Desert(
@@ -9,9 +10,45 @@ data class Desert(
     val id: Int? = null,
     val fields: Set<DesertField>,
 ) {
+
     companion object {
         fun create12PlayerHexagon(): Desert {
-            TODO()
+            val fields = mutableSetOf<DesertField>()
+
+            val desertValuesByDistance = mapOf(
+                0 to Pair(-8, 40),
+                1 to Pair(-7, 37),
+                2 to Pair(-6, 29),
+                3 to Pair(-5, 19),
+                4 to Pair(-4, 11),
+                5 to Pair(-3, 5),
+                6 to Pair(-2, 2),
+                7 to Pair(-1, 1),
+            )
+
+            for (x in -7..7) {
+                for (y in -14..14) {
+                    val distance = abs(x) + abs(y)
+                    if (distance % 2 == 0 && distance / 2 <= 7) {
+                        val values = desertValuesByDistance[distance / 2] ?: Pair(0, 0)
+
+                        val desertField = DesertField(
+                            id = "${'B' + x + 7}${(y + 14 + 2) / 2}",
+                            water = values.first,
+                            spice = values.second,
+                            effectiveWeapon = Weapon.LEGION, // TODO
+                            neighbours = setOf(),
+                            startingField = false, // TODO
+                        )
+
+                        println("($x, $y): $desertField")
+
+                        fields.add(desertField)
+                    }
+                }
+            }
+
+            return Desert(fields)
         }
     }
 }
