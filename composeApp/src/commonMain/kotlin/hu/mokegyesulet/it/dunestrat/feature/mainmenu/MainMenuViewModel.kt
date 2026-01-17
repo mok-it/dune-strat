11f5b2beeb313ed15fb3f68fa5deab0d730684f1
Tamási Máté <matetamasi@protonmail.com>
2026-01-17 20:10:21 +0100

    Add simple AuthStatus class

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenuViewModel.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenuViewModel.kt
@@ -2,8 +2,8 @@ package hu.mokegyesulet.it.dunestrat.feature.mainmenu
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import hu.mokegyesulet.it.dunestrat.backend.AuthStatus
 import hu.mokegyesulet.it.dunestrat.backend.SupabaseAuth
-import io.github.jan.supabase.auth.status.SessionStatus
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -22,7 +22,7 @@ class MainMenuViewModel : ViewModel() {
     }
 
     val isLoggedIn: Flow<Boolean>
-        get() = SupabaseAuth.sessionStatus.map {
-            it is SessionStatus.Authenticated
+        get() = SupabaseAuth.authStatus.map {
+            it is AuthStatus.Authenticated
         }
 }
