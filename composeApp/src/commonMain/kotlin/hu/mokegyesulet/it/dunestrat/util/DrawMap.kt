40acb428ce43e0c459ea94c715a007e17a767fb4
László Görömbey <laszlo.gorombey@gmail.com>
2026-02-01 16:52:55 +0000

    Very basic svg generation

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/util/DrawMap.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/util/DrawMap.kt
@@ -1,10 +1,9 @@
 package hu.mokegyesulet.it.dunestrat.util
 
-fun hexDrawer(unitVector: UnitVector): String {
-    val vecX = AbsoluteVector(144.0, -83.13843876)
-    val vecY = AbsoluteVector(0.0, 166.2768775)
+import hu.mokegyesulet.it.dunestrat.model.Desert
 
-    val transformVector = unitVector.x * vecX + unitVector.y * vecY
+fun hexDrawer(unitVector: UnitVector): String {
+    val transformVector = getTransformVector(unitVector)
 
     return "<polygon style =\"fill:none;stroke:#000000;stroke-width:2.5px\" points = " +
 
@@ -14,4 +13,33 @@ fun hexDrawer(unitVector: UnitVector): String {
         "${52 + transformVector.x },${16.8615612366939 + transformVector.y} " +
         "${148 + transformVector.x},${16.8615612366939 + transformVector.y } " +
         "${196 + transformVector.x},${100 + transformVector.y }\"/>"
+}
+
+fun getTransformVector(unitVector: UnitVector): AbsoluteVector {
+    val vecX = AbsoluteVector(144.0, -83.13843876)
+    val vecY = AbsoluteVector(0.0, 166.2768775)
+    return unitVector.x * vecX + unitVector.y * vecY
+}
+
+fun getSimpleSvg(desert: Desert): String {
+    val builder = StringBuilder()
+    builder.append("<svg xmlns=\"http://www.w3.org/2000/svg\">\n")
+
+    for (field in desert.fields) {
+        val unitVector = UnitVector.fromDisplayCoordinate(field.id)
+        val transformVector = getTransformVector(unitVector)
+        builder.append(hexDrawer(unitVector) + "\n")
+        builder.append(
+            "<text x=\"${transformVector.x + 50}\" y=\"${transformVector.y + 50}\">${field.id}</text>\n",
+        )
+    }
+
+    builder.append("</svg>")
+    return builder.toString()
+}
+
+fun main() {
+    val desert = Desert.create12PlayerHexagon()
+    val svgString = getSimpleSvg(desert)
+    println(svgString)
 }
