b34b0f1a2f357162b6b0b1dafe33bcddd9cd0a10
user93482 <nimrodrumi@gmai.com>
2026-02-15 18:30:46 +0100

    random scheme

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/App.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/App.kt
@@ -1,14 +1,26 @@
 package hu.mokegyesulet.it.dunestrat
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
 import hu.mokegyesulet.it.dunestrat.navigation.NavGraph
+import hu.mokegyesulet.it.dunestrat.ui.AppTheme
+import hu.mokegyesulet.it.dunestrat.ui.md_theme_dark_background
+import hu.mokegyesulet.it.dunestrat.ui.md_theme_light_background
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        NavGraph()
+    Surface(color = if(isSystemInDarkTheme()){
+        md_theme_light_background
+    }else{
+        md_theme_dark_background
+    },tonalElevation = 5.dp) {
+        AppTheme {
+            NavGraph()
+        }
     }
+
 }
