aa30b86c75e47df4c21b2c475c54e89544660a78
user93482 <nimrodrumi@gmai.com>
2026-02-15 19:34:29 +0100

    ability to make any shape

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/App.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/App.kt
@@ -6,17 +6,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import hu.mokegyesulet.it.dunestrat.navigation.NavGraph
 import hu.mokegyesulet.it.dunestrat.ui.AppTheme
-import hu.mokegyesulet.it.dunestrat.ui.md_theme_dark_background
-import hu.mokegyesulet.it.dunestrat.ui.md_theme_light_background
+import hu.mokegyesulet.it.dunestrat.ui.dark_background
+import hu.mokegyesulet.it.dunestrat.ui.light_background
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     Surface(color = if(isSystemInDarkTheme()){
-        md_theme_light_background
+        light_background
     }else{
-        md_theme_dark_background
+        dark_background
     },tonalElevation = 5.dp) {
         AppTheme {
             NavGraph()
