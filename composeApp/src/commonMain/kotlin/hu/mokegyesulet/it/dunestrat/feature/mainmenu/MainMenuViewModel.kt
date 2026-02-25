3cd7a4b497948179513f125319e7668c186377e7
cstomi74 <tomicsontos27@gmail.com>
2026-02-25 19:13:50 +0100

    A main menu alapvetően szerintem el van rendezve, magán a kinézetén kell még csiszolni bőven, illetve a grid a közepén csak egy placeholder, oda még be kell ügyeskedni az aktuális game statet.

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenuViewModel.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenuViewModel.kt
@@ -1,5 +1,6 @@
 package hu.mokegyesulet.it.dunestrat.feature.mainmenu
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import hu.mokegyesulet.it.dunestrat.backend.AuthStatus
@@ -29,4 +30,15 @@ class MainMenuViewModel : ViewModel() {
         get() = SupabaseAuth.authStatus.map {
             it is AuthStatus.Authenticated
         }
+
+    val gameCount = mutableStateOf(0)
+    val games = SupabaseRepository.getGames()
+    sealed class Event() {
+        data object CreateGame : Event()
+    }
+    fun onEvent(event: Event) {
+        when (event) {
+            is Event.CreateGame -> gameCount.value += 1
+        }
+    }
 }
