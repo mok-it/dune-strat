16baceb6905d007176dbede7c8b3661876f854b6
László Görömbey <laszlo.gorombey@gmail.com>
2026-03-14 11:05:17 +0000

    Navigation

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/navigation/NavGraph.kt
@@ -1,18 +1,21 @@
 package hu.mokegyesulet.it.dunestrat.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import hu.mokegyesulet.it.dunestrat.feature.init.InitScreen
 import hu.mokegyesulet.it.dunestrat.feature.mainmenu.MainMenu
 import hu.mokegyesulet.it.dunestrat.feature.playerstep.PlayerStepInputScreen
 import hu.mokegyesulet.it.dunestrat.feature.testdata.TestDataScreen
 
 @Composable
-fun NavGraph(navController: NavHostController = rememberNavController()) {
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+    toRoute: NavBackStackEntry.() -> Screen.PlayerStepInput,
+) {
     NavHost(
         navController = navController,
         startDestination = Screen.MainMenu,
