8788d1b1d749d726a5735610307dc3528f393f4f
László Görömbey <laszlo.gorombey@gmail.com>
2026-06-19 15:30:57 +0200

    Minimális főmenü

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenu.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/feature/mainmenu/MainMenu.kt
@@ -1,16 +1,18 @@
 package hu.mokegyesulet.it.dunestrat.feature.mainmenu
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,136 +24,81 @@ import org.jetbrains.compose.resources.painterResource
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainMenu(
-    onInputMoves: () -> Unit,
-    onStat: () -> Unit,
+    onInputMovesClick: (Int) -> Unit,
+    onStat: (Int) -> Unit,
+    onNewGameClick: () -> Unit,
 ) {
     val viewModel = viewModel { MainMenuViewModel() }
     val games by viewModel.games.collectAsStateWithLifecycle()
-    val gameCount = 0
-    var isExpanded by remember { viewModel.isExpanded }
-    var selectedGame by remember { mutableStateOf<Game?>(null) }
-    val deserts by viewModel.deserts.collectAsStateWithLifecycle()
-    val selectedDesert by viewModel.selectedGame.collectAsState()
+    val selectedGame by viewModel.selectedGame
+    val players by viewModel.players
 
-    Row {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
         LazyColumn {
-            items(games) { game: Game ->
-                Card { Text(game.name) }
-            }
-        }
-        Column(
-            Modifier.padding(10.dp).fillMaxHeight(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start,
-        ) {
-            Button(
-                onClick = { viewModel.onEvent(MainMenuViewModel.Event.CreateGame) },
-                modifier = Modifier.width(240.dp)
-                    .height(60.dp),
-
-            ) {
-                Text(text = "Új játék")
-            }
-            Text(text = "Létrehozott játékok száma: $gameCount")
-
-            ExposedDropdownMenuBox(
-                expanded = isExpanded,
-                onExpandedChange = { isExpanded = !isExpanded },
-            ) {
-                TextField(
-                    label = { Text("Játék kiválasztása") },
-                    value = selectedGame?.name ?: "Select a game",
-                    onValueChange = {},
-                    readOnly = true,
-                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(isExpanded) },
-                    modifier = Modifier.menuAnchor(),
-                )
-
-                ExposedDropdownMenu(
-                    expanded = isExpanded,
-                    onDismissRequest = { isExpanded = false },
-                ) {
-                    for (g in games) {
-                        DropdownMenuItem(
-                            text = { Text(text = g.name) },
-                            onClick = {
-                                viewModel.onEvent(MainMenuViewModel.Event.ExpandMenu)
-                                viewModel.onEvent(MainMenuViewModel.Event.SelectGame(g))
-                            },
-                        )
+            item {
+                Button(onClick = onNewGameClick) {
+                    Row {
+                        Icon(Icons.Filled.Add, contentDescription = "Add new game")
+                        Text(text = "Új játék")
                     }
                 }
-                Text(text = "Selected ID: ${selectedGame?.id ?: "none"}")
             }
 
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .padding(5.dp)
-                    .weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-
-            ) {
-                Image(
-                    painter = painterResource(Res.drawable.grid),
-                    contentDescription = null,
-                    modifier = Modifier.background(Color.Yellow)
-                        .size(width = 600.dp, height = 600.dp),
-                )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
+            items(games) { game: Game ->
+                Card(
+                    border = if (selectedGame == game) {
+                        BorderStroke(2.dp, Color.Red)
+                    } else {
+                        null
+                    },
+                    modifier = Modifier.clickable {
+                        viewModel.onEvent(MainMenuViewModel.Event.SelectGame(game))
+                    }.padding(4.dp),
                 ) {
-                    Image(
-                        painter = painterResource(Res.drawable.grid),
-                        contentDescription = null,
-                        modifier = Modifier.size(width = 400.dp, height = 400.dp),
-
+                    Text(
+                        text = game.name,
+                        modifier = Modifier.padding(8.dp),
                     )
                 }
             }
-
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .width(300.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.CenterHorizontally,
-
+        }
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painterResource(Res.drawable.grid),
+                contentDescription = "Grid image",
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Button(
+                onClick = { onInputMovesClick(selectedGame?.id ?: -1) },
+                enabled =
+                    selectedGame != null,
             ) {
-                Button(
-                    onClick = onInputMoves,
-                    modifier = Modifier.padding(10.dp)
-                        .width(200.dp)
-                        .height(50.dp)
-                        .align(Alignment.End),
-                ) {
-                    Text(text = "Lépések felvétele")
+                Text(text = "Lépések bevitele")
+            }
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+                    .wrapContentHeight(),
+            ) {
+                items(players.toList()) { player ->
+                    Text(text = "${player.id}: ${player.water} víz, ${player.spice} fűszer")
                 }
-                Text(
-                    text = "Statisztikák: ",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.width(200.dp),
-
-                )
-
-//            Text(
-//                text = "${viewModel.waterAmount}",
-//                textAlign = TextAlign.Left,
-//            )
-
-                Spacer(
-                    modifier = Modifier.height(8.dp)
-                        .weight(10f),
-                )
-
-                Button(
-                    onClick = onStat,
-                    modifier = Modifier.padding(10.dp),
-                ) {
-                    Text(text = "STAT")
-                }
+            }
+            Button(
+                onClick = { onStat(selectedGame?.id ?: -1) },
+                enabled =
+                    selectedGame != null,
+            ) {
+                Text(text = "Statisztika")
             }
         }
     }
