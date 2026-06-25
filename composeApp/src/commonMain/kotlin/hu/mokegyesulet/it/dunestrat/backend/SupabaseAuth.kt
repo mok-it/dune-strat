e41f1971ec164b04f21a2c6541592261aeee220e
László Görömbey <laszlo.gorombey@gmail.com>
2026-06-25 20:41:59 +0200

    Change login redirect

--- composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/backend/SupabaseAuth.kt
+++ composeApp/src/commonMain/kotlin/hu/mokegyesulet/it/dunestrat/backend/SupabaseAuth.kt
@@ -9,7 +9,8 @@ import kotlinx.coroutines.flow.map
 object SupabaseAuth : Auth {
     private val auth = SupabaseClientProvider.client.auth
 
-    override suspend fun logIn() = auth.signInWith(Google)
+    override suspend fun logIn() =
+        auth.signInWith(provider = Google, redirectUrl = "https://mok-it.github.io/dune-strat/")
     override val authStatus get(): Flow<AuthStatus> = auth.sessionStatus.map { s ->
         if (s is SessionStatus.Authenticated) {
             AuthStatus.Authenticated(
